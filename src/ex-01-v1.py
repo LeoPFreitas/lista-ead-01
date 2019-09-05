39047a67a0985c9ca8234d7dd54424c9a3d50adf
LeoPFreitas <leonardofreitasdev@outlook.com>
2019-09-05 18:13:22 -0300

    Update ex 01

--- src/ex-01-v1.py
+++ src/ex-01-v1.py
@@ -3,21 +3,6 @@ Problema:
 Escreva um programa que remove a primeira ocorrencia de uma letra de uma string.
 A string e a letra devem ser fornecidas pelo usuario.
 '''
-# ============ MODO O1 ============
-# * REMOÇÃO POR FATIAMENTO
-# * Neste modo, foi utilizado a string frase e não houve como remover com o del...
-#   seria por ela (frase) não ser uma lista????
-# * Eu utilizei a fatiamento para não imprimir a letra, mas ela não foi excluida
-#   da string de fato.
-
-# ============ EM ABERTO ============
-# * Utilizar método para colocar tudo em minúsculo e evitar o problema de letra
-#   minúscula na verificxação
-
-# ============ IMPORTANTE ============
-# * VERSÃO 01 DESTE PROBLEMA, VER V2 PARA VERSÃO 02
-
-# ============ INICIO ============
 
 # Recebendo a string do usuário
 frase = input("Digite uma frase: ")
@@ -37,7 +22,8 @@ while (indice < len(frase)) and (not encontrou):
 
 # Imprimindo
 if encontrou:
-    print("A frase original é '{}' e a frase com a letra removida é '{}'".format(
-        frase, frase[0:indice]+frase[indice+1:]))
+    fraseFinal = frase[0:indice] + frase[indice+1:]
+    print("A frase original é '{}' e a frase com a letra '{}' removida é '{}'".format(
+        frase, letra, fraseFinal))
 else:
     print("A letra {} não ocorre na frase {}".format(letra, frase))
